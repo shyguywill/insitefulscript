7e806fd5e8e23ae96ed385521f7e0ae10006fa98
Sayo Williams <sayo@sayos-mbp.home>
2020-12-18 21:15:39 +0000

    configure data capure

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var data = {}
 
+const contentWidth = [...document.body.children].reduce( 
+    (a, el) => Math.max(a, el.getBoundingClientRect().right), 0) 
+    - document.body.getBoundingClientRect().x
+
+const pageDims = {height: document.body.scrollHeight, width: Math.min(document.body.scrollWidth, contentWidth)}
+const entryTime = Date().toLocaleString()
+
 function confirmExit(){}
 
 function captureData(e) {}
 
 function logClick(e) {
-    const name = e.path[0].localName
-    const innerText = e.path[0].innerText
     const type = e.type
+    const coords = { x: e.x, y: e.y }
+    const name = e.target.localName
+    const innerText = e.target.innerText
+    const navigated = e.target.href
     
-    console.log(name, innerText, type)
+    console.log(type, coords, pageDims, name, innerText, navigated)
     console.dir(e)
 }
+function logScroll(e){
+    console.log(e)
+}
+
 document.addEventListener('click', logClick);
+window.addEventListener('scroll', logScroll)
 
 /* 
 click events to take note of:
